e7cda6be1e50c2964e0c0714901d672a7ade6154
bloodfi5h <jimmy@aiadvisors.co.uk>
2024-10-15 14:16:07 +0100

    done!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://aiastro-dev.netlify.app/"
+  site: "https://aiastro-dev.netlify.app/",
+  integrations: [preact()]
 });
